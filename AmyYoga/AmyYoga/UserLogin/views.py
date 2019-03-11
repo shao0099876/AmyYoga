9cc44a5c658ce36ecc90587c8f2228608be406ef
shao0099876 <shao0099876@outlook.com>
2019-03-11 16:20:18 +0800

    添加未找到用户名的逻辑

--- AmyYoga/AmyYoga/UserLogin/views.py
+++ AmyYoga/AmyYoga/UserLogin/views.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponsePermanentRedirect, HttpResponse
 from django.shortcuts import render
 from .forms import LoginForm
+from .models import Customer as UserDB
 
 
 # Create your views here.
@@ -8,12 +10,17 @@ def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username=form.cleaned_data['username']
-            password=form.cleaned_data['password']
-            return HttpResponse(username+' '+password)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            try:
+                user = UserDB.objects.get(username=username)
+            except(ObjectDoesNotExist):
+                return HttpResponse("ObjectDoesNotExist")
+            # else:
+
     else:
         if request.session.get('loginStatus', default=None) is not None:
             return HttpResponsePermanentRedirect('/')
         else:
-            form=LoginForm()
-            return render(request,'loginUI.html',{'loginForm':form})
+            form = LoginForm()
+            return render(request, 'loginUI.html', {'loginForm': form})
