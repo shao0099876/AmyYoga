62890a3fc9ba3da6d397f188723a3c761c515c1f
shao0099876 <shao0099876@outlook.com>
2019-03-21 16:19:21 +0800

    添加错误信息显示

--- AmyYoga/AmyYoga/UserLogin/views.py
+++ AmyYoga/AmyYoga/UserLogin/views.py
@@ -12,11 +12,13 @@ def login(request):  # 用户登录功能视图函数
         if loginForm.is_valid():  # 解析表单
             username = loginForm.cleaned_data['username']  # 获得表单内用户名
             password = loginForm.cleaned_data['password']  # 获得表单内密码
+            print(username,password)
             user = UserDB()  # 创建空用户对象
             try:
                 user = UserDB.objects.get(username=username)  # 尝试查询该用户
             except ObjectDoesNotExist:
-                return HttpResponse("UsernameDoesNotExist")  # 如果没查询到，返回用户名不存在信息
+                errormessage = "UsernameDoesNotExist"  # 如果没查询到，返回用户名不存在信息
+                return render(request, 'loginUI.html', locals())
             if user.checkAuthority(password):  # 如果认证成功
                 request.session['loginStatus'] = 'Online'  # 设置登录状态为在线
                 if user.isAdministrator():  # 根据身份标志设置当前连接用户的权限标志
@@ -27,7 +29,7 @@ def login(request):  # 用户登录功能视图函数
                     "Logined,loginStatus=" + request.session.get("loginStatus") + " authority=" + request.session.get(
                         "authority"))
             else:
-                return HttpResponse("PasswordWrong")  # 返回密码错误信息
+                errormessage = "PasswordWrong"  # 返回密码错误信息
     else:  # 如果是普通访问（GET方法）
         if request.session.get('loginStatus', default=None) is not None:  # 检查用户是否已经登录
             return HttpResponsePermanentRedirect('/')  # 如果已经登录，跳转到首页
