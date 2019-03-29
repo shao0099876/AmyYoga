f1e7e6c30626071b56775f277d992a3ccd0dade8
shao0099876 <shao0099876@outlook.com>
2019-03-29 18:22:15 +0800

    修正管理查看个人信息的访问限制问题

--- AmyYoga/AmyYoga/superuser_message/views.py
+++ AmyYoga/AmyYoga/superuser_message/views.py
@@ -1,14 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from UserLogin import models
 
+from Tools import SessionManager
+
+
 def superusermessage(request):
-    #if request.method == 'POST':  # 如果请求为表单提交
-    #   return HttpResponse("ok")
+    if SessionManager.isLogouted(request):
+        return HttpResponseRedirect("/login/")
     user_list = models.PersonalInformation.objects.all()
     return render(request, 'vipmessage.html', locals())
 
 def moremessage(request, user):
+    if SessionManager.isLogouted(request):
+        return HttpResponseRedirect("/login/")
     user_list = models.PersonalInformation.objects.filter(username=user)
     #return HttpResponse(user)
     return render(request, 'moremessage.html',locals() )
