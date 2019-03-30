43f0a4271ff4efad2e5cb5db5a9d27cf2f0f72e3
shao0099876 <shao0099876@outlook.com>
2019-03-30 08:55:56 +0800

    Issue#48修改

--- AmyYoga/AmyYoga/superuser_message/views.py
+++ AmyYoga/AmyYoga/superuser_message/views.py
@@ -8,12 +8,16 @@ from Tools import SessionManager
 def superusermessage(request):
     if SessionManager.isLogouted(request):
         return HttpResponseRedirect("/login/")
+    if not SessionManager.isAdministrator():
+        return HttpResponseRedirect("/")
     user_list = models.PersonalInformation.objects.all()
     return render(request, 'vipmessage.html', locals())
 
 def moremessage(request, user):
     if SessionManager.isLogouted(request):
         return HttpResponseRedirect("/login/")
+    if not SessionManager.isAdministrator():
+        return HttpResponseRedirect("/")
     user_list = models.PersonalInformation.objects.filter(username=user)
     #return HttpResponse(user)
     return render(request, 'moremessage.html',locals() )
