b158991ec8688dcbc3d02828305b458f9d7fd274
shao0099876 <shao0099876@outlook.com>
2019-04-08 14:38:42 +0800

    创建测试集

--- AmyYoga/AmyYoga/UserLogin/tests.py
+++ AmyYoga/AmyYoga/UserLogin/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
+from django.test import TestCase,Client
+from Database.models import Customer
 # Create your tests here.
+class UserLoginTestCase(TestCase):
+    def setUp(self):
+        Customer.objects.create(username="test_admin",authoritySignal=True,password="test_admin_password")
+        Customer.objects.create(username="test_customer",password="test_customer_password")
+    def test_user_uplogined_visit(self):
+        c=Client()
+        response=c.get('/login/')
