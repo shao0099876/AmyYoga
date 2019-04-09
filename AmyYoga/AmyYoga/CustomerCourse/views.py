ed86342a97d94f9a649cea490b7d36058c60b38e
Littlle <1326049040@qq.com>
2019-04-09 15:44:58 +0800

    客户查看我的课程

--- AmyYoga/AmyYoga/CustomerCourse/views.py
+++ AmyYoga/AmyYoga/CustomerCourse/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from Database.models import BuyCourse as buyCourseDB #购买课程记录，需要修改
-from Database.models import BuyedCourse as buyedCourseDB #已消费课程记录，需要修改
+from Database.models import BuyCourse as buyCourseDB #已支付课程记录，需要修改
+from Database.models import BuyedCourse as buyedCourseDB #未支付课程记录，需要修改
 from Tools import SessionManager,FormsManager
 
 # Create your views here.
 
-def customercourse(request):  # 向页面输出订单
+def customercourse(request):  # 向页面输出当前用户已支付的订单信息
     username = SessionManager.getUsername(request) #获取当前登录的用户名字
+    user=buyCourseDB.objects.filter(username=username)
+
+
 
     return render(request, 'customercourseUI.html', locals())  # 渲染页面 按照课程名排序
+
+
+def uncustomercourse(request): #向页面输出当前用户未支付的订单信息
+    username = SessionManager.getUsername(request)  # 获取当前登录的用户名字
+
+    return render(request, 'uncustomercourseUI.html', locals())  # 渲染页面 按照课程名排序
