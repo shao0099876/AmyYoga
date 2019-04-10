c96f5db9bb3b4e32cd00bfa2ecf366a34c6729ba
shao0099876 <shao0099876@outlook.com>
2019-04-10 15:06:09 +0800

    完成UserLogin的自动化测试编写

--- AmyYoga/AmyYoga/UserLogin/tests.py
+++ AmyYoga/AmyYoga/UserLogin/tests.py
@@ -9,21 +9,40 @@ class UserLoginTestCase(TestCase):
         Customer.objects.create(username="test_admin",authoritySignal=True,password="test_admin_password")
         Customer.objects.create(username="test_customer",password="test_customer_password")
     def test_user_unlogined_visit(self):
-        c=Client()
-        session=c.session
-        session['LoginStatus']=SESSION_LOGINSTATUS_OFFLINE
-        response=c.get('/login/')
+        c = self.client
+        session = self.client.session
+        session['LoginStatus'] = 'Offline'
+        session.save()
+        response=c.get("/login/")
         self.assertEqual(response.status_code, 200)
     def test_info_lost_username(self):
-        c=Client()
+        c=self.client
         response=c.post('/login/',{'username':'','password':'test'})
         self.assertFormError(response,"loginForm","username","This field is required.")
     def test_info_lost_password(self):
-        c=Client()
+        c=self.client
         response=c.post('/login/',{'username':'test_admin','password':''})
         self.assertFormError(response,"loginForm","password","This field is required.")
     def test_user_logined_visit(self):
-        c=Client(follow=True)
-        c.session['LoginStatus']=SESSION_LOGINSTATUS_ONLINE
-        response=c.get("/login/")
-        self.assertRedirects(response,"/",)
+        c=self.client
+        session=self.client.session
+        session['LoginStatus']='Online'
+        session.save()
+        response=c.get("/login/",follow=True)
+        self.assertRedirects(response,"/")
+    def test_login_wrongusername(self):
+        c=self.client
+        response = c.post('/login/', {'username': 'test', 'password': 'test'})
+        self.assertFormError(response, "loginForm", None, "用户名不存在")
+    def test_login_wrongpassword(self):
+        c=self.client
+        response=c.post('/login/',{'username':'test_admin','password':'test'})
+        self.assertFormError(response, "loginForm", None, "密码错误")
+    def test_admin_login_successfully(self):
+        c=self.client
+        response=c.post('/login/',{'username':'test_admin','password':'test_admin_password'})
+        self.assertRedirects(response,'/administratorloginedindex/')
+        session = self.client.session
+        self.assertEqual("test_admin",session.get("Username"))
+        self.assertEqual("Administrator",session.get("Authority"))
+        self.assertEqual("Online",session.get("LoginStatus"))
