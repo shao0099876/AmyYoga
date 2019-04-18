3f9a04b81e1b997d20880aabd9a8588270c109ba
shao0099876 <shao0099876@outlook.com>
2019-04-18 10:27:43 +0800

    完成用例编写

--- AmyYoga/AmyYoga/AmyYoga/settings.py
+++ AmyYoga/AmyYoga/AmyYoga/settings.py
@@ -35,15 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'UserLogin',
-    'CustomerRegister',
-    'ChangePassword',
-    'CustomerCompleteInformation',
-    'Index',
-    'superuser_message',
     'Database',
-    'CustomerCourse',
-    'admin_CourseMessage',
 ]
 
 MIDDLEWARE = [
