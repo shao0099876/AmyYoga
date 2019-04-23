bdda3c4a459e6a5897c08602364b056441b9508f
Littlle <1326049040@qq.com>
2019-04-23 15:04:35 +0800

    查看课程管理员与客户合并

--- AmyYoga/AmyYoga/Course/views.py
+++ AmyYoga/AmyYoga/Course/views.py
@@ -9,15 +9,31 @@ from .forms import AddCourseForm, ModCourseForm
 def viewCourse(request):  # 管理员查看课程信息
     '''这个功能换成通用的，管理员访问在模板生成时多加个修改的按钮'''
     sessionManager = SessionManager(request)
-    courses = Course.objects.filter(course_flag=True)  # 查询在使用的课程信息
-    return render(request, 'coursemessageUI.html', {'order': courses})
+    if sessionManager.isAdministrator(): #如果是管理员登陆
+        courses = Course.objects.all()  # 查询全部课程信息
+        people1 = 'guanliyuan'
+
+    else: #如果是客户登陆
+        courses = Course.objects.filter(course_flag=True)  # 查询在使用的课程信息
+        people1 = 'kehu'
+
+    return render(request, 'coursemessageUI.html', {'order': courses, 'people': people1})
 
 
 def viewCourseDetails(request, coursename):  # 显示课程的详细信息
     '''修改此处功能，换成显示课程的课程名，简介等信息'''
     sessionManager = SessionManager(request)
-    detailcourse = BuyRecord.objects.filter(coursename=coursename, valid=True)  # 查询这个课程的所有订单，包括付钱和没付钱的
-    return render(request, 'detailmessageUI.html', {'order1': detailcourse})
+    if sessionManager.isAdministrator(): #如果是管理员登陆
+        courses = Course.objects.get(coursename=coursename)  # 查询当前课程信息,为了后面显示详细信息
+        detailcourse = BuyRecord.objects.filter(coursename=coursename)  # 查询这个课程的所有订单（包括付钱和没付钱的）
+        people2 = 'guanliyuan'
+
+    else: #如果是客户登陆
+        username = sessionManager.getUsername()  # 获取当前登录的用户名字
+        courses = Course.objects.get(coursename=coursename)  # 查询当前课程信息,为了后面显示详细信息
+        detailcourse = BuyRecord.objects.filter(username=username,coursename=coursename)  # 查询这个用户关于这门课的订单状态（付钱和没付钱的）
+        people2 = 'kehu'
+    return render(request, 'detailmessageUI.html', {'people':people2,'courses':courses,'order1': detailcourse})
 
 
 def addCourse(request):  # 管理员增加课程信息
