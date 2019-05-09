ccbdd5695f33b10e336e4e9e7840e288abf72433
Vector-Change <1366401414@qq.com>
2019-05-09 23:54:34 +0800

    Update

--- AmyYoga/AmyYoga/CourseUsed/urls.py
+++ AmyYoga/AmyYoga/CourseUsed/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = {
-    path('', views.CourseUsed),
+    path('manager/', views.CourseUsed),
+    path('user/',views.UserCourseUsed),
     #path('<username>',views.moremessage_username),
     #path('<coursename>',views.moremessage_coursename),
 }
